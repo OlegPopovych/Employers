f935e391c806f11b60bcfda0533a7984f73611f1
Oleg <olezhkapopovych@gmail.com>
2022-08-08 23:40:59 +0300

    changed to CLASS EmployersListItem

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './employers-list-item.css';
 
-const EmployersListItem = ({ name, salary, increase }) => {
-	let classses = "list-group-item d-flex justify-content-between";
-	if (increase) {
-		classses += " increase";
+class EmployersListItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			increase: false
+		}
 	}
-	return (
-		//"list-group-item d-flex justify-content-between "
-		<li className={classses}>
-			<span className="list-group-item-label">{name}</span>
-			<input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-			<div className='d-flex justify-content-center align-items-center'>
-				<button type="button"
-					className="btn-cookie btn-sm ">
-					<i className="fas fa-cookie"></i>
-				</button>
 
-				<button type="button"
-					className="btn-trash btn-sm ">
-					<i className="fas fa-trash"></i>
-				</button>
-				<i className="fas fa-star"></i>
-			</div>
-		</li>
-	)
+	onIncrease = () => {
+		this.setState(({ increase }) => ({  // дужки навколо тіла функції щоб не писати return
+			increase: !increase
+		}))
+	}
+
+	render() {
+		const { name, salary } = this.props;
+		const { increase } = this.state;
+
+		let classses = "list-group-item d-flex justify-content-between";
+		if (increase) {
+			classses += " increase";
+		}
+		return (
+			//"list-group-item d-flex justify-content-between "
+			<li className={classses}>
+				<span className="list-group-item-label">{name}</span>
+				<input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+				<div className='d-flex justify-content-center align-items-center'>
+					<button type="button"
+						className="btn-cookie btn-sm "
+						onClick={this.onIncrease}>
+						<i className="fas fa-cookie"></i>
+					</button>
+
+					<button type="button"
+						className="btn-trash btn-sm ">
+						<i className="fas fa-trash"></i>
+					</button>
+					<i className="fas fa-star"></i>
+				</div>
+			</li>
+		)
+	}
 }
 
 export default EmployersListItem;
