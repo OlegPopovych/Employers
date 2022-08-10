ba63e60ae96febe99d6325fa8663ee6a9df249c6
Oleg <olezhkapopovych@gmail.com>
2022-08-11 00:15:41 +0300

    lesson 127

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
@@ -9,24 +9,59 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
-	const data = [
-		{ name: "Joh2n C.", salary: 800, increase: false, id: 1 },
-		{ name: "Alex3 G.", salary: 3000, increase: true, id: 2 },
-		{ name: "JCarl B.", salary: 5000, increase: false, id: 3 }
-	]
-	return (
-		<div className="app">
-			<AppInfo />,
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: "Joh2n C.", salary: 800, increase: false, id: 1 },
+				{ name: "Alex3 G.", salary: 3000, increase: true, id: 2 },
+				{ name: "JCarl B.", salary: 5000, increase: false, id: 3 }
+			]
+		}
+	}
+
+	deleteItem = (id) => {
+		this.setState(({ data }) => {
+			//спосіб 2    1 спосіб внизу
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+
+		})
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<AppInfo />,
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+				<EmployersList
+					data={this.state.data}
+					onDelete={this.deleteItem} /*цей пропс передаэться нижчому по рівню елементу!!!*/ />
+				<EmployersAddForm />
 			</div>
-			<EmployersList data={data} />
-			<EmployersAddForm />
-		</div>
-	);
+		);
+	}
 }
 
 
 export default App;
+
+
+
+
+/*//спосіб №1 видалити з масиву елемент по його індексу
+const index = data.findIndex(elem => elem.id == id);
+const before = data.slice(0, index);
+const after = data.slice(index + 1);
+
+const newArr = [...before, ...after];   
+
+
+return {
+	data: newArr
+}   */
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -4,14 +4,16 @@ import EmployersListItem from '../employers-list-item/employers-list-item';
 
 import './employers-list.css';
 
-const EmployersList = ({ data }) => {
+const EmployersList = ({ data, onDelete }) => {
 	const elements = data.map(item => {
 		return (
 			<EmployersListItem
 				key={item.id}
 				name={item.name}
 				salary={item.salary}
-				increase={item.increase} />
+				increase={item.increase}
+				onDelete={() => onDelete(item.id)} //цей пропс передаэться нижчому по рівню елементу!!!
+			/>
 		)
 	});
 
